3f58711f0f5c96bed0a31ea3390b2a2ff6168143
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2020-03-13 01:45:06 +0530

    js

---
+++ script.js
@@ -0,0 +1,27 @@
+var a;
+console.log(a);
+
+// data types
+
+var name ="yasmeen";
+console.log(name);
+var age =34;
+console.log(age);
+
+
+var msg1 ="yasmeen";
+var age =24
+var sum = (msg1 + " " + age);
+console.log(sum);
+
+
+var msg1 = "There are ";
+var numStudents = 25;
+var msg2 = " students in class";
+
+console.log(msg1 + numStudents + msg2);
+
+
+
+
+
